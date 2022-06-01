37ced56593e8bf90b0e3f78e5309413d30e05425
Daniil Loktev <lokt.daniil@gmail.com>
2022-06-01 22:47:14 +0300

    Fix type in parameters

--- src/main/java/com/alphabank/restapi/clients/CurrencyClient.java
+++ src/main/java/com/alphabank/restapi/clients/CurrencyClient.java
@@ -6,14 +6,12 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import java.util.Date;
-
 @FeignClient("currency")
 public interface CurrencyClient {
 
     @RequestMapping(method = RequestMethod.GET, value = "/historical/{date}.json?app_id={appId}&symbols={symbols}")
     GetHistoricalExchangeRatesResponse getHistoricalExchangeRates(
-            @PathVariable("date") Date date,
+            @PathVariable("date") String date,
             @PathVariable("appId") String appId,
             @PathVariable("symbols") String symbols
     );
