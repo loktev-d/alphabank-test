4df130eb9a62ceb62b174920e9d8864937f148cd
Daniil Loktev <lokt.daniil@gmail.com>
2022-06-03 21:44:17 +0300

    Refactor Currency client

--- src/main/java/com/alphabank/restapi/clients/CurrencyClient.java
+++ src/main/java/com/alphabank/restapi/clients/CurrencyClient.java
@@ -2,23 +2,21 @@ package com.alphabank.restapi.clients;
 
 import com.alphabank.restapi.dto.currency.GetHistoricalExchangeRatesResponse;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "currency", url = "${currency-api-url}")
+@FeignClient(name = "currency", url = "${currency.api-url}")
 public interface CurrencyClient {
 
-    @RequestMapping(method = RequestMethod.GET, value = "/latest.json?app_id={appId}&symbols={symbols}")
+    @GetMapping("/latest.json?app_id=${currency.api-key}")
     GetHistoricalExchangeRatesResponse getLatestExchangeRates(
-            @PathVariable("appId") String appId,
-            @PathVariable("symbols") String symbols
+            @RequestParam("symbols") String symbols
     );
 
-    @RequestMapping(method = RequestMethod.GET, value = "/historical/{date}.json?app_id={appId}&symbols={symbols}")
+    @GetMapping("/historical/{date}.json?app_id=${currency.api-key}")
     GetHistoricalExchangeRatesResponse getHistoricalExchangeRates(
             @PathVariable("date") String date,
-            @PathVariable("appId") String appId,
-            @PathVariable("symbols") String symbols
+            @RequestParam("symbols") String symbols
     );
 }
