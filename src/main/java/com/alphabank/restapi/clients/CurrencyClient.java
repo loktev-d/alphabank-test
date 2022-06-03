b2dc38eb5bbc9707d9a7b9f5d3acd51c2693b396
Daniil Loktev <lokt.daniil@gmail.com>
2022-06-03 10:53:36 +0300

    Add getLatest endpoint to Currency client

--- src/main/java/com/alphabank/restapi/clients/CurrencyClient.java
+++ src/main/java/com/alphabank/restapi/clients/CurrencyClient.java
@@ -9,6 +9,12 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @FeignClient(name = "currency", url = "${currency-api-url}")
 public interface CurrencyClient {
 
+    @RequestMapping(method = RequestMethod.GET, value = "/latest.json?app_id={appId}&symbols={symbols}")
+    GetHistoricalExchangeRatesResponse getLatestExchangeRates(
+            @PathVariable("appId") String appId,
+            @PathVariable("symbols") String symbols
+    );
+
     @RequestMapping(method = RequestMethod.GET, value = "/historical/{date}.json?app_id={appId}&symbols={symbols}")
     GetHistoricalExchangeRatesResponse getHistoricalExchangeRates(
             @PathVariable("date") String date,
