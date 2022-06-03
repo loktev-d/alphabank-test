1c009176e1821c2b6367f98aa2b3d0ad0f07e621
Daniil Loktev <lokt.daniil@gmail.com>
2022-06-03 22:54:00 +0300

    Refactor controller

--- src/main/java/com/alphabank/restapi/controllers/MainController.java
+++ src/main/java/com/alphabank/restapi/controllers/MainController.java
@@ -3,14 +3,14 @@ package com.alphabank.restapi.controllers;
 import com.alphabank.restapi.services.CurrencyService;
 import com.alphabank.restapi.services.GifService;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public record MainController(CurrencyService currencyService, GifService gifService) {
 
-    @GetMapping("/api/{currencyCode}")
-    public String getCurrencyRateGif(@PathVariable String currencyCode) {
+    @GetMapping("/api")
+    public String getCurrencyRateGif(@RequestParam(value = "code", defaultValue = "${currency.default-currency-code}") String currencyCode) {
         var gifQuery = currencyService.isCurrencyRateRisingToday(currencyCode) ? "rich" : "broke";
 
         return String.format("<img src=\"%s\" />", gifService.getRandomGifUrlBySearch(gifQuery));
