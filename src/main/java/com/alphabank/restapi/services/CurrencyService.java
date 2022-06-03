efeb896c9d87338dcf33a84a7821d5b968769f11
Daniil Loktev <lokt.daniil@gmail.com>
2022-06-03 22:00:03 +0300

    Refactor Currency service

--- src/main/java/com/alphabank/restapi/services/CurrencyService.java
+++ src/main/java/com/alphabank/restapi/services/CurrencyService.java
@@ -1,24 +1,29 @@
 package com.alphabank.restapi.services;
 
 import com.alphabank.restapi.clients.CurrencyClient;
-import com.alphabank.restapi.configurations.ApiKeysConfig;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 @Service
-public record CurrencyService(CurrencyClient currencyClient, ApiKeysConfig apiKeysConfig) {
+public record CurrencyService(CurrencyClient currencyClient) {
 
     public boolean isCurrencyRateRisingToday(String currencyCode) {
         var yesterdayRate = currencyClient.getHistoricalExchangeRates(
                 LocalDate.now().minusDays(1).format(DateTimeFormatter.ofPattern("yyyy-MM-dd")),
-                apiKeysConfig.getCurrencyApiKey(),
                 currencyCode
         ).rates().get(currencyCode.toUpperCase());
 
+        if (yesterdayRate == null)
+            throw new ResponseStatusException(
+                    HttpStatus.BAD_REQUEST,
+                    String.format("Currency with code %s not found", currencyCode)
+            );
+
         var todayRate = currencyClient.getLatestExchangeRates(
-                apiKeysConfig.getCurrencyApiKey(),
                 currencyCode
         ).rates().get(currencyCode.toUpperCase());
 
